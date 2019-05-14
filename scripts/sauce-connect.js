5e1141359f520c7cc8758779d1708a72a85d9b62
Blade Barringer <blade.barringer@getbraintree.com>
2019-05-14 20:42:16 +0000

    Adjust travis.yml

--- scripts/sauce-connect.js
+++ scripts/sauce-connect.js
@@ -1,11 +1,11 @@
 var dotenv = require('dotenv');
 var sauceConnectLauncher = require('sauce-connect-launcher');
 
-dotenv.config();
+var config = dotenv.config();
 
 sauceConnectLauncher({
-  username: process.env.SAUCE_USERNAME,
-  accessKey: process.env.SAUCE_ACCESS_KEY
+  username: config.SAUCE_USERNAME,
+  accessKey: config.SAUCE_ACCESS_KEY
 }, function (err, sauceConnectProcess) {
   if (err) {
     console.error(err.message);
