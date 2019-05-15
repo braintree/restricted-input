2899aa9ebab1dd0aca0ed44e45c20b1f1a642ce7
Blade Barringer <blade.barringer@getbraintree.com>
2019-05-15 21:48:07 +0000

    localhost again

--- spec/restricted_input_spec.rb
+++ spec/restricted_input_spec.rb
@@ -5,7 +5,7 @@ PORT ||= ENV['PORT'] || 3099
 
 describe 'Restricted Input' do
   before :each do
-    visit "http://#{HOSTNAME}:#{PORT}"
+    visit "http://localhost:#{PORT}"
   end
 
   # describe 'for number' do
