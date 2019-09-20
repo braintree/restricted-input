f3db6e16268abf3101fa38ccab2bd1272ad6425b
Blade Barringer <blade.barringer@getbraintree.com>
2019-09-20 19:22:21 +0000

    Address comments from @akbraun

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,64 +1,56 @@
+const uuid = require('uuid/v4');
+const browserstack = require('browserstack-local');
+
 // Stop node from complaining about fake memory leaks at higher concurrency
-require("events").defaultMaxListeners = 20;
-require("dotenv").config();
+require('events').defaultMaxListeners = 20;
+require('dotenv').config();
 
-const path = require("path");
-const uuid = require("uuid/v4");
 const localIdentifier = uuid();
-
-const screenResolution = "1920x1080";
-const browserstack = require("browserstack-local");
+const screenResolution = '1920x1080';
 
 const defaultCapabilities = {
-  "browserstack.debug": true,
-  "browserstack.local": true,
-  "browserstack.networkLogs": true,
-  "browserstack.localIdentifier": localIdentifier
+  'browserstack.debug': true,
+  'browserstack.local': true,
+  'browserstack.networkLogs': true,
+  'browserstack.localIdentifier': localIdentifier
 };
 
 const desktopCapabilities = {
   ...defaultCapabilities,
-  os: "windows",
-  os_version: "10",
-  resolution: screenResolution
-};
-const windows7Capabilities = {
-  ...defaultCapabilities,
-  os: "windows",
-  os_version: "7",
+  os: 'windows',
+  os_version: '10',
   resolution: screenResolution
 };
 
 const mobileCapabilities = {
   ...defaultCapabilities,
   real_mobile: true,
-  "browserstack.appium_version": "1.14.0"
+  'browserstack.appium_version': '1.14.0'
 };
 
 let capabilities = [
   {
     ...desktopCapabilities,
     browserName: 'Google Chrome',
-    browser: "chrome",
-    browser_version: "76",
-    "browserstack.console": "info"
+    browser: 'chrome',
+    'browserstack.console': 'info'
   },
   // TODO mobile browsers are having trouble with browser.keys
   //{
   //  ...mobileCapabilities,
   //  browserName: 'iPhone XS Safari',
-  //  device: "iPhone XS",
-  //  os_version: "13"
+  //  device: 'iPhone XS',
+  //  os_version: '13'
   //},
   //{
   //  ...mobileCapabilities,
   //  browserName: 'Google Pixel 3 Chrome',
-  //  device: "Google Pixel 3",
-  //  os_version: "9.0",
+  //  device: 'Google Pixel 3',
+  //  os_version: '9.0',
   //  chromeOptions: {
   //    prefs: {
   //      // disable chrome's annoying password manager, may be unnec
-  //      "profile.password_manager_enabled": false,
+  //      'profile.password_manager_enabled': false,
   //      credentials_enable_service: false,
   //      password_manager_enabled: false
   //    }
@@ -66,31 +58,29 @@ let capabilities = [
   //},
   {
     ...desktopCapabilities,
-    browser: "IE",
+    browser: 'IE',
     browserName: 'IE 11',
-    browser_version: "11.0",
+    browser_version: '11.0',
     'browserstack.selenium_version' : '3.5.2'
   },
   {
     ...desktopCapabilities,
     browserName: 'Microsoft Edge',
-    browser: "edge",
-    browser_version: "18.0"
+    browser: 'edge',
+    browser_version: '18.0'
   },
   {
     ...desktopCapabilities,
     browserName: 'Firefox',
-    browser: "firefox",
-    browser_version: "69.0",
-    "browserstack.console": "info"
+    browser: 'firefox',
+    'browserstack.console': 'info'
   },
   {
     ...desktopCapabilities,
     browserName: 'Desktop Safari',
-    browser: "safari",
-    browser_version: "12.1",
-    os: "OS X",
-    os_version: "Mojave"
+    browser: 'safari',
+    os: 'OS X',
+    os_version: 'Mojave'
   }
 ];
 
@@ -105,38 +95,35 @@ if (process.env.ONLY_BROWSER) {
 }
 
 exports.config = {
-  runner: "local",
+  runner: 'local',
   user: process.env.BROWSERSTACK_USERNAME,
   key: process.env.BROWSERSTACK_ACCESS_KEY,
   browserstackLocal: true,
-  specs: require("fs")
-    .readdirSync("./test/integration")
+  specs: require('fs')
+    .readdirSync('./test/integration')
     .map(f => `./test/integration/${f}`),
-  exclude: [
-    // 'path/to/excluded/files'
-  ],
   maxInstances: 6,
   capabilities,
   sync: true,
-  logLevel: "error",
+  logLevel: 'error',
   deprecationWarnings: true,
   bail: 0,
   baseUrl: process.env.HOST,
   waitforTimeout: 20000,
   connectionRetryTimeout: 90000,
   connectionRetryCount: 1,
-  services: ["browserstack"],
-  framework: "mocha",
+  services: ['browserstack'],
+  framework: 'mocha',
   mochaOpts: {
     timeout: 60000
   },
-  reporters: ["spec"],
+  reporters: ['spec'],
   reportOptions: {
-    outputDir: "./"
+    outputDir: './'
   },
   onPrepare() {
     /* eslint-disable no-console */
-    console.log("Connecting local");
+    console.log('Connecting local');
     return new Promise((resolve, reject) => {
       exports.bs_local = new browserstack.Local();
       exports.bs_local.start(
@@ -148,7 +135,7 @@ exports.config = {
           if (error) return reject(error);
           console.log(`Connected with localIdentifier=${localIdentifier}`);
           console.log(
-            "Testing in the following browsers:",
+            'Testing in the following browsers:',
             capabilities
               .map(
                 browser =>
@@ -156,7 +143,7 @@ exports.config = {
                     ? `${browser.device}@${browser.os_version}`
                     : `${browser.browser}@${browser.browser_version}`
               )
-              .join(", ")
+              .join(', ')
           );
 
           return resolve();
