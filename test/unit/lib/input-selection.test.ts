081dcfa8619d2741b05c9bd7e9351717c2b987bb
Blade Barringer <blade.barringer@getbraintree.com>
2020-07-09 20:02:45 +0000

    Update test

--- test/unit/lib/input-selection.test.ts
+++ test/unit/lib/input-selection.test.ts
@@ -2,36 +2,23 @@ import { get as getCurrentSelection } from "../../../src/lib/input-selection";
 
 describe("getCurrentSelection", function () {
   describe("element type", function () {
-    it("returns selection range for input", function () {
-      const element = document.createElement("input");
+    it.each(["input", "textarea"])(
+      "returns selection range for %s",
+      (elementType) => {
+        const element = document.createElement(elementType) as HTMLInputElement;
 
-      // set a value so the element has text to select
-      element.value = "asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasd";
-      element.setSelectionRange(10, 23);
+        // set a value so the element has text to select
+        element.value = "asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasd";
+        element.setSelectionRange(10, 23);
 
-      const result = getCurrentSelection(element);
+        const result = getCurrentSelection(element);
 
-      expect(result).toEqual({
-        start: 10,
-        end: 23,
-        delta: 13,
-      });
-    });
-
-    it("returns selection range for textarea", function () {
-      const element = document.createElement("textarea");
-
-      // set a value so the element has text to select
-      element.value = "asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasd";
-      element.setSelectionRange(10, 23);
-
-      const result = getCurrentSelection(element);
-
-      expect(result).toEqual({
-        start: 10,
-        end: 23,
-        delta: 13,
-      });
-    });
+        expect(result).toEqual({
+          start: 10,
+          end: 23,
+          delta: 13,
+        });
+      }
+    );
   });
 });
