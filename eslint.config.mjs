9aac3aede45b793b11aabc9e4e315bc7510d5949
Derek Mayer <dermayer@paypal.com>
2026-02-20 11:01:47 -0600

    Fixing ESLint config and adding jest-util dev dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,14 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import braintreeClientConfig from "eslint-config-braintree/client";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default defineConfig([globalIgnores(["**/dist/", "**/dist-app/"]), {
-    extends: compat.extends("braintree/client"),
-
+export default defineConfig([
+  globalIgnores(["**/dist/", "**/dist-app/"]),
+  ...braintreeClientConfig,
+  {
     languageOptions: {
-        globals: {
-            define: true,
-        },
+      globals: {
+        define: true,
+      },
     },
-}]);
+  },
+]);
